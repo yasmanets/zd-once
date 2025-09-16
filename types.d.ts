4fefd3fa12d4c36dd4c8799ce49f5d90843f2ced
Pedro Narciso García Revington <p.revington@gmail.com>
2025-09-16 11:43:28 +0200

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,6 @@
 export declare function once<T extends (...args: any[]) => any>(
   fn: T
-): (...args: Parameters<T>) => ReturnType<T> | undefined;
+): (...args: Parameters<T>) => ReturnType<T>;
 
 export declare function onceStrict<T extends (...args: any[]) => any>(
   fn: T
